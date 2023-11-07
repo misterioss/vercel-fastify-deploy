66f54a634d1505e565be01c6f186a778f5464914
serhii_s <serhii.sakhniuk@gmail.com>
2023-11-07 16:55:15 +0200

    Try #4

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify'
 
 (async () => {
+    console.log('Start server')
     const server = fastify()
 
     server.get('/ping', () => 'pong 🏓')
+
+    server.get('/', () => {
+        console.log('test simple route')
+    })
+
     await server.ready()
     console.log(server.printRoutes())
     await server.listen({port: 3000})
